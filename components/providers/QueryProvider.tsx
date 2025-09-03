14ba1eba9a47a43d120560065efb4480d9186634
Damilola Bada <damilolabada@Damilolas-Mac-mini.local>
2025-09-03 14:12:52 +0200

    initialised project, setup trpc, implemented queryClient and tested with mockData

---
+++ components/providers/QueryProvider.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { trpc } from "@/server/trpcClient";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/react-query";
+import { useState } from "react";
+
+export default function QueryProvider({ children }: { children: React.ReactNode }) {
+    const [queryClient] = useState(() => new QueryClient());
+    const [trpcClient] = useState(() =>
+        trpc.createClient({
+            links: [
+                httpBatchLink({
+                    url: "http://localhost:3000/api/trpc",
+                }),
+            ],
+        })
+    );
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <trpc.Provider client={trpcClient} queryClient={queryClient}>
+                {children}
+            </trpc.Provider>
+        </QueryClientProvider>
+    );
+}
---
+++ server/routers/user.ts
@@ -0,0 +1,10 @@
+import { procedure, router } from "../trpc";
+
+export const userRouter = router({
+    getUsers: procedure.query(() => {
+        return [
+            { id: 1, name: "John Doe" },
+            { id: 2, name: "Jane Smith" }
+        ];
+    })
+})
